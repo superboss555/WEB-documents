cac58f22db5b57e2905971a3382efe57362c29fc
Artem Bobylev <artematrr@gmail.com>
2025-01-20 19:23:22 +0400

    clean up client code

--- Client/client.go
+++ Client/client.go
@@ -9,40 +9,36 @@ import (
 )
 
 func main() {
-	// Открываем HTML-страницу регистрации в браузере
-	err := exec.Command("cmd", "/c", "start", "index.html").Start() // Для Windows
-	// err := exec.Command("open", "index.html").Start() // Для macOS
+	err := exec.Command("cmd", "/c", "start", "index.html").Start() // Windows
+	// err := exec.Command("open", "index.html").Start() // macOS
 	if err != nil {
-		log.Fatal("Ошибка открытия браузера:", err)
+		log.Fatal("Ошибка при открытия браузера:", err)
 	}
 
-	// Укажите адрес вашего WebSocket-сервера
 	url := "ws://localhost:8080/ws"
 
-	// Подключаемся к WebSocket-серверу
 	conn, _, err := websocket.DefaultDialer.Dial(url, nil)
 	if err != nil {
-		log.Fatal("Ошибка подключения:", err)
+		log.Fatal("Ошибка при подключении:", err)
 	}
+	
 	defer conn.Close()
 
 	log.Println("Подключение к WebSocket-серверу установлено")
 
-	// Запускаем горутину для обработки входящих сообщений
 	go func() {
 		for {
 			_, msg, err := conn.ReadMessage()
 			if err != nil {
-				log.Println("Ошибка чтения сообщения:", err)
+				log.Println("Ошибка при чтения сообщения:", err)
 				return
 			}
 			log.Printf("Получено сообщение: %s\n", msg)
 		}
 	}()
 
-	// Бесконечный цикл для поддержания работы клиента
 	for {
-		time.Sleep(1 * time.Second) // Задержка для предотвращения перегрузки процессора
+		time.Sleep(1 * time.Second) 
 
 	}
 }
